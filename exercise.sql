46f21b7dcbb358171ed3de85ddc3b89d86e5b00c
Anouar ElGhazaoui <2080049@talnet.nl>
2020-03-04 12:03:40 +0100

    done

--- exercise.sql
+++ exercise.sql
@@ -1,7 +1,9 @@
-INSERT INTO planeten(naam) 
-VALUES
-	("Zon"),
-    ("Mercurius"), 
-    ("Venus"), 
-    ("Aarde"), 
-    ("Mars");
+TRUNCATE planeten;
+
+INSERT INTO planeten(naam, diameter, afstand_totnaam_zon, massa)
+VALUES 
+("Zon", 1392000, 0, 332946), 
+("Mercurius", 4880, 57910000, 0.1), 
+("Venus", 12104,108208930, 0.9), 
+("Aarde", 12756, 149597870, 1), 
+("Mars", 6794, 227936640, 0.1);
